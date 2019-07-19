cbbfbb3e1b5507ed0f6b8c265a58a17fc42712b8
Fouad Messaia <f.messaia@veritas-data.de>
2019-07-19 23:07:54 +0200

    + New extensions

---
+++ Messaia.Net.Common/CollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿///-----------------------------------------------------------------
+///   Author:         Messaia
+///   AuthorUrl:      http://messaia.com
+///   Date:           04.01.2019 17:19:43
+///   Copyright (©)   2019, VERITAS DATA GmbH, all Rights Reserved. 
+///                   No part of this document may be reproduced 
+///                   without VERITAS DATA GmbH's express consent. 
+///-----------------------------------------------------------------
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// CollectionExtensions class.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue = default(TValue))
+        {
+            return dict.TryGetValue(key, out TValue value) ? value : defaultValue;
+        }
+    }
+}
---
+++ Messaia.Net.Common/DecimalExtensions.cs
@@ -0,0 +1,46 @@
+﻿///-----------------------------------------------------------------
+///   Author:         Messaia
+///   AuthorUrl:      http://messaia.com
+///   Date:           04.01.2019 17:19:43
+///   Copyright (©)   2019, VERITAS DATA GmbH, all Rights Reserved. 
+///                   No part of this document may be reproduced 
+///                   without VERITAS DATA GmbH's express consent. 
+///-----------------------------------------------------------------
+namespace System
+{
+    /// <summary>
+    /// DecimalExtensions class.
+    /// </summary>
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// Formats a decimal to a price string
+        /// </summary>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        public static string FormatPrice(this decimal price)
+        {
+            return string.Format("{0:0},<sup>{1:00}</sup>", price.IntPart(), price.FractionalPart());
+        }
+
+        /// <summary>
+        /// Gets the integer part of a decimal
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int IntPart(this decimal number)
+        {
+            return (int)number;
+        }
+
+        /// <summary>
+        /// Gets the fractional part of a decimal
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int FractionalPart(this decimal number)
+        {
+            return (int)((number - (int)number) * 100);
+        }
+    }
+}
